656a1a57a51af2c11f4b45f5a7793ed62e7b052f
Md Saddam Hossan <saddamhossan.tkg@gmail.com>
2025-08-02 10:29:00 +0000

    datatype-summary

---
+++ datatype-summary.js
@@ -0,0 +1,34 @@
+// primitive
+
+// 7 type : String, Number, Boolearn, Null, Undefined, Symbol, BigInt.
+
+// const score = 100
+// const score = 100.3
+
+const isLoggedIn = false
+const outsideTemp = null
+let userEmail;
+
+const id = symbol('123')
+const anotherId = Symbol('123')
+
+console.log(id === anotherId);
+const bigNumber = 3665995855985555855n
+
+// Reference (Non Primitive) : array, Objects, Functions
+
+const heros = ["shaktiman", "naagraj", "doga"]
+
+let myObj = {
+    name: "saddam"
+    age: 22,
+}
+
+const myFuntion = function (){
+    console.log("hello world");
+    
+}
+
+console.log(typeof);
+
+
